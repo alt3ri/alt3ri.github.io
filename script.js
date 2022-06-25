2aa6c815f538ebc19aa6e702133687462a43c75c
alt <95161279+alt3ri@users.noreply.github.com>
2022-06-26 01:28:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,28 +20,28 @@ var video = document.getElementById("myVideo");
 
 document.addEventListener('keydown', function() {
     if (event.keyCode == 123) {
-        alert("This function has been disabled to prevent you from stealing my code!");
+        alert("yo!");
         return false;
     } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-        alert("This function has been disabled to prevent you from stealing my code!");
+        alert("ayo!");
         return false;
     } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
-        alert("This function has been disabled to prevent you from stealing my code!");
+        alert("heyo!");
         return false;
     } else if (event.ctrlKey && event.keyCode == 85) {
-        alert("This function has been disabled to prevent you from stealing my code!");
+        alert("o!");
         return false;
     }
 }, false);
 
 if (document.addEventListener) {
     document.addEventListener('contextmenu', function(e) {
-        alert("This function has been disabled to prevent you from stealing my code!");
+        alert("a!");
         e.preventDefault();
     }, false);
 } else {
     document.attachEvent('oncontextmenu', function() {
-        alert("This function has been disabled to prevent you from stealing my code!");
+        alert("e!");
         window.event.returnValue = false;
     });
 }
